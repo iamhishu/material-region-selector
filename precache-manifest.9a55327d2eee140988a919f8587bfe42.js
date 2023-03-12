7554ded615451e1c16aeae503e527c4ec1614262
hitesh <45461407+iamhishu@users.noreply.github.com>
2023-03-12 09:28:52 +0530

    Updates

---
+++ precache-manifest.9a55327d2eee140988a919f8587bfe42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a768deab479fff67fff229f9e6cabcc1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f61cea401a22e9f16281",
+    "url": "./static/css/main.8a3df49a.chunk.css"
+  },
+  {
+    "revision": "a6b010c12f0416c18372",
+    "url": "./static/js/2.418e878a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.418e878a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f61cea401a22e9f16281",
+    "url": "./static/js/main.3b2dfee2.chunk.js"
+  },
+  {
+    "revision": "36f39be7474ad2d6569f",
+    "url": "./static/js/runtime-main.0d29a6f4.js"
+  }
+]);
